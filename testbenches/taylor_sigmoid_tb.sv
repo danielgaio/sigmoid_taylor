3168269288911bbfcbb078a231fa02ddea7facfa
Daniel <danielelielgaio@gmail.com>
2021-08-01 09:07:46 -0300

    feat(Testbench): :sparkles: Gerar valores exatos
    Foi implementado a lógica que gera 4096 resultados exatos (bom grau de precisão). Os resultados são armazenados em arquivo .txt

--- testbenches/taylor_sigmoid_tb.sv
+++ testbenches/taylor_sigmoid_tb.sv
@@ -7,13 +7,13 @@ module psoa_sigmoid_tb();
 	logic [11:0] f_x_tb;
 	logic clk_tb;
 
-	shortreal 	generated_results [1000];
-	shortreal 	expected_results [1000];
+	shortreal 	generated_results [4096];
+	shortreal 	expected_results [4096];
 	logic 			j;
 	shortreal 	passo, passo_abs, temp, temp2; // 32 bit
 	int 				k, i;
 	shortreal 	erro_medio, max_error;
-	int 				clk_counter;
+	int 				clk_counter, file_descriptor;
 
 	// modulo teste
 	sigmoid_taylor sigmoid_taylor_DUT(
@@ -40,59 +40,72 @@ module psoa_sigmoid_tb();
 
 	// estimulos
 	initial begin
-		fork
+		//fork
 		    // x_tb = 12'b0010_10000000;	// sig(2.5)=0.924 | em bin: 000_11101101
-		    x_tb = 12'b0011_00000000; // +3
+		    // x_tb = 12'b0011_00000000; // +3
 				// x_tb = 12'b1101_00000000; // -3 -> 0.0474
 				// x_tb = 12'b0001_00000000; // +1
-				#40
-		    $display("f_x = %b", f_x_tb);
-		join
-		$stop;
+				//#40
+		    //$display("f_x = %b", f_x_tb);
+		//join
+		//$stop;
 
 		// secao para geracao de valores de entrada
-		fork
-			passo = -8;
-			// intervalo de [-8,+8]
-			for (i=0; i<=1000; i++) begin
-				// se input for negativo injetar valor absoluto e guardar (1 - result)
-				if (passo < 0) begin
-					passo_abs = -passo;
-					x_tb = passo_abs*1024;
-				// se input for positivo
-				end else begin
-					x_tb = passo*1024;
-				end
+		// fork
+		// 	passo = -8;
+		// 	// intervalo de [-8,+8]
+		// 	for (i=0; i<=1000; i++) begin
+		// 		// se input for negativo injetar valor absoluto e guardar (1 - result)
+		// 		if (passo < 0) begin
+		// 			passo_abs = -passo;
+		// 			x_tb = passo_abs*1024;
+		// 		// se input for positivo
+		// 		end else begin
+		// 			x_tb = passo*1024;
+		// 		end
 
-				$display("x= %d", x_tb);
-				#40
+		// 		$display("x= %d", x_tb);
+		// 		#40
 
-				if (passo < 0) begin
+		// 		if (passo < 0) begin
 
-					//if (clk_counter > 1) begin
-						generated_results [i] = 1 - (shortreal'(f_x_tb)/shortreal'(1024));
-						$display("generated result [%d]: %f", i, generated_results[i]);
-					//end
+		// 			//if (clk_counter > 1) begin
+		// 				generated_results [i] = 1 - (shortreal'(f_x_tb)/shortreal'(1024));
+		// 				$display("generated result [%d]: %f", i, generated_results[i]);
+		// 			//end
 
-				end else begin
+		// 		end else begin
 
-					generated_results [i] = (shortreal'(f_x_tb)/shortreal'(1024));
-					$display("generated result [%d]: %f", i, generated_results[i]);
+		// 			generated_results [i] = (shortreal'(f_x_tb)/shortreal'(1024));
+		// 			$display("generated result [%d]: %f", i, generated_results[i]);
 
-				end
-				passo += 0.016;
-			end
-		join
-		$stop;
+		// 		end
+		// 		passo += 0.016;
+		// 	end
+		// join
+		// $stop;
 
 		// calculando valor preciso e guardando em expected_results
 		fork
-			passo = -8;
-			for (k=0; k<1000; k++) begin
+			// Cria controlador do arquivo txt
+			file_descriptor = $fopen("./../../../testbenches/log_exact_values.txt", "a");
+			if (file_descriptor)
+				$display("Arquivo aberto com sucesso: %0d", file_descriptor);
+			else
+				$display("Erro na abertura do arquivo: %0d", file_descriptor);
+
+			passo = -7.999;
+			for (k=0; k<4096; k++) begin
 				expected_results[k] = 1.0/(1.0+(2.718281828**(-passo)));
+				$display("passo: %f", passo);
+				$fwrite(file_descriptor, "passo: %f\n", passo);
 				$display("expected_results[%d]: %f", k, expected_results[k]);
-				passo += 0.016;
+				$fwrite(file_descriptor, "expected_results[%d]: %f\n", k, expected_results[k]);
+				passo += 0.00390;
 			end
+			//Encerra controlador do arquivo
+			$fwrite(file_descriptor, "\n\n");
+			$fclose(file_descriptor);
 		join
 		$stop;
 
